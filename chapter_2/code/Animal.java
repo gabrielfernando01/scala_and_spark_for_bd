8e37ac63b362138ce57ee93a9ef77d6a5f5f9497
gabrielfernando01 <fernando.rozepeda@gmail.com>
2025-03-04 22:48:01 -0600

    create Animal.java

---
+++ chapter_2/code/Animal.java
@@ -0,0 +1,31 @@
+// =====================================================
+
+public class Animal {
+    public String animalName = "null";
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+    public String getAnimalName() {
+        return animalName;
+    }
+    public boolean isAnimalNameSet() {
+        if(getAnimalName() == "notset") {
+            return false;
+    } else {
+        return true;
+    }
+}
+
+public static void main(String[] args) {
+    Animal obj = new Animal();
+    boolean flag = false;
+    obj.setAnimalName("dog");
+    flag = obj.isAnimalNameSet();
+    System.out.println(flag);
+    obj.setAnimalName("notset");
+    flag = obj.isAnimalNameSet();
+    System.out.println(flag);
+    }
+}
+
+
